b3114d71d0e57110796f0124654d019f9598ee2b
Xavier Mendoza <mendozaxavier567@gmail.com>
2026-07-28 19:55:14 -0700

    Implement student transcript view

--- src/components/student/Transcript.jsx
+++ src/components/student/Transcript.jsx
@@ -41,9 +41,27 @@ const Transcript = () => {
   return (
     <>
       <h3>Transcript</h3>
-      <p>To be implemented.  Display a table showing the course a student has taken.
-        The table columns are given in headers.
-      </p>
+      <Messages response={message} />
+      <table className="Center">
+        <thead>
+          <tr>
+            {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr key={course.enrollmentId}>
+              <td>{course.year}</td>
+              <td>{course.semester}</td>
+              <td>{course.courseId}</td>
+              <td>{course.sectionId}</td>
+              <td>{course.title}</td>
+              <td>{course.credits}</td>
+              <td>{course.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
     </>
   );
